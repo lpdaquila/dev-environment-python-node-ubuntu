efa348ae4e64a9731c3ee1d058670b020c747a7b
Lucas D'aquila <99849531+lpdaquila@users.noreply.github.com>
2025-05-03 20:10:56 +0000

    setup-dev-env.sh criado

---
+++ setup-dev-env.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+set -e
+
+# Atualizar pacotes e sistema
+sudo apt update && sudo apt upgrade -y
+
+# Instalando ferramentas essenciais para compilação e desenvolvimento..."
+sudo apt install -y \
+	gcc make libssl-dev \
+    build-essential \
+	zlib1g-dev \
+	libbz2-dev \
+	libreadline-dev \
+	libsqlite3-dev \
+	llvm \
+    libffi-dev \
+    software-properties-common \
+	libncurses5-dev libncursesw5-dev \
+	xz-utils tk-dev liblzma-dev \
+    ca-certificates
+
+# Instalar ferramentas utilitárias e de linha de comando
+sudo apt install -y \
+    curl \
+    wget \
+    git \
+    unzip \
+	perl \
+	dkms \
+    nano \
+    vim \
+    tree \
+    neofetch \
+    man \
+    lsb-release \
+    tmux \
+    rsync \
+    jq \
+    lsof
+
+# Instalar suporte a aplicações gráficas (X11) (Opcional para WSL)
+sudo apt install -y \
+    x11-apps \
+    dbus-x11 \
+    libxkbcommon-x11-0 \
+    libgl1-mesa-glx \
+    libegl1-mesa \
+    libxcb-keysyms1 \
+    libxcb-icccm4 \
+    libxcb-image0 \
+    libxcb-shm0 \
+    libxcb-randr0 \
+    libxcb-render-util0 \
+    libxcb-xinerama0 \
+    libsm6 \
+    libxext6 \
+    libxrender1 \
+    gedit 
+
+# Instalar ferramentas de monitoramento de sistema e rede
+sudo apt install -y \
+    htop \
+    iotop \
+    iftop \
+    nethogs \
+    iptraf-ng \
+    bmon \
+    net-tools \
+    dstat \
+    iputils-ping \
+    traceroute \
+    dnsutils \
+    telnet \
+    nmap \
+    netcat \
+    sysstat
+
+# Instalar Python 3, pip e venv, etc
+sudo apt install -y \
+    python3 \
+    python3-pip \
+    python3-venv \
+    python3-dev \
+	python3-openssl
+
+#Instalar Node.js (LTS) e npm"
+curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+sudo apt install -y nodejs
+
+# Instalar pacotes globais do Node.js"
+sudo npm install -g \
+    typescript \
+    yarn \
+    pnpm \
+    eslint \
+    nodemon \
+    serve
+	
+# Instalar o pyenv
+curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+echo "Não feche esse terminal e siga as instruções do Pyenv acima!!!!"
+
+echo "Ambiente de desenvolvimento configurado com sucesso!!!!!!"
+
+# Opcionais: zsh 
